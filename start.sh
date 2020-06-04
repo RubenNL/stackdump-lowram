f10e26ad4deb146da3f366235d3544afaebea92d
tigger4life <66224674+tigger4life@users.noreply.github.com>
2020-06-04 18:09:11 -0400

    Update start.sh
    Added spacing in between the steps. Modified step 4  to create header.xml. Original step 4 created file Posts.xml then step 5 deleted Posts.xml which meant the header was never added to the final file.
    
    Modified step 9 to create a tmp directory. Previously running as user or root would cause this step to fail as it appeared to try to write to either /tmp or /var/tmp 
    
    Modified step 10 to copy header.xml to Posts.xml then add 3 spaces before copying combinedSorted.xml
    
    Modified step 11 to remove header.xml and /home/$USERNAME/tmp directory

--- start.sh
+++ start.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 npm install #installs event-stream. required for parsing huge files in nodejs
+
 echo step1
 grep -F 'PostTypeId="1"' Posts.xml | grep -Fv 'AnswerCount="0"' | tee questions.xml | grep -F 'Score="-' | cut -d'"' -f2 > deleteQuestions.ids #removes all questions without answers, writes that to questions.xml. also writes all questions with negative score to deleteQuestions.ids
+
 echo step2
 grep -Fv 'Score="-' questions.xml > positiveQuestions.xml #writes all positive questions to positivequestions.xml
 rm questions.xml
+
 echo step3
 grep -F 'PostTypeId="2"' Posts.xml > answers.xml #writes all answers to answers.xml
-
 mv Posts.xml Posts.xml-old
+
 echo step4
-head -n2 Posts.xml-old > Posts.xml #XML headers required. took me 4 days to figure this out.
+head -n2 Posts.xml-old > header.xml #XML headers required. took me 4 days to figure this out.
+
 echo step5
 tail -n1 Posts.xml-old > lastLine.xml
-rm Posts.xml
 
 echo step6
 node remove-downvoted-answers.js #uses deletequestions.ids and answers.xml, writes all answers to non-negative questions to answersWithoutDeletedQuestions.xml, with the id of the question at the beginning, to make sorting easier.
-
 rm deleteQuestions.ids answers.xml
+
 echo step7
 node questions-number-beginning.js #uses positiveQuestions.xml, puts the id of the question at the beginning of the line, to make sorting easier. writes to questionsStartId.xml
 rm positiveQuestions.xml
@@ -28,13 +31,19 @@ cat questionsStartId.xml answersWithoutDeletedQuestions.xml > combinedNotSorted.
 rm questionsStartId.xml answersWithoutDeletedQuestions.xml
 
 echo step9
-sort --parallel=2 -k 1n -n -o combinedSorted.xml combinedNotSorted.xml #sorts everything. let it do its thing, takes a couple hours.
+mkdir /home/$USERNAME/tmp #creates a tmp dir. add your username
+sort -T /home/$USERNAME/tmp --parallel=2 -k 1n -n -o combinedSorted.xml combinedNotSorted.xml #sorts everything in tmp dir. let it do its thing, takes a couple hours.
 rm combinedNotSorted.xml
+
 echo step10
-cut -f2- combinedSorted.xml >> Posts.xml #removes the id at the beginning
+cp header.xml  Posts.xml
+tail -n +3 combinedSorted.xml >> Posts.xml
 rm combinedSorted.xml
+
 echo step11
-cat lastLine.xml >> Posts.xml #ads the lastline to the file
+cat lastLine.xml >> Posts.xml #adds the lastline to the file.
 rm lastLine.xml
+rm header.xml
+rm -rf /home/$USERNAME/tmp
 
 echo DONE!
