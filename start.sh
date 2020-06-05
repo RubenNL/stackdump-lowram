ba2d5fe2c6c2d1049c316b4a5201a36075a6cce4
Ruben <15885455+RubenNL@users.noreply.github.com>
2020-06-05 13:17:00 +0200

    development version, not tested on the large stackoverflow dump, only on smaller ones. this seems to be faster.

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
-grep -F 'PostTypeId="1"' Posts.xml | grep -Fv 'AnswerCount="0"' | awk '{split($0,a,"\""); print a[2] $0}'>combinedNotSorted.xml
-grep -F 'PostTypeId="2"' Posts.xml | awk '{split($0,a,"\""); print a[6] $0}' >> combinedNotSorted.xml
 mv Posts.xml Posts.xml-old
 head -n2 Posts.xml-old > Posts.xml
-tail -n1 Posts.xml-old > lastLine.xml
+(grep -F 'PostTypeId="1"' Posts.xml-old | grep -Fv 'AnswerCount="0"' | awk '{split($0,a,"\""); print a[2]"\t"$0}'; grep -F 'PostTypeId="2"' Posts.xml-old | awk '{split($0,a,"\""); print a[6]"\t"$0}') | sort --parallel=2 -k 1n -n | cut -f2- >> Posts.xml
+tail -n1 Posts.xml-old >> Posts.xml
 rm Posts.xml-old
-sort --parallel=2 -k 1n -n combinedNotSorted.xml | cut -f2- >> Posts.xml
-rm combinedNotSorted.xml
-cat lastLine.xml >> Posts.xml
-rm lastLine.xml
